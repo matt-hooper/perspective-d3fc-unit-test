27252e92316b87d69018e19d3c0658ba40eb5d5e
Matthew Hooper <mhooper@scottlogic.co.uk>
2019-02-11 09:57:04 +0000

    first commit

---
+++ crossAxis.spec.js
@@ -0,0 +1,11 @@
+import { scale, domain } from '../perspective/packages/perspective-viewer-d3fc/src/js/axis/crossAxis';
+
+describe('First Test', () => {
+    const c = null;
+
+    test('should be null if no data has been specified', () => {
+        expect(c).toBeNull();
+    });
+
+    console.log("Here");
+});
